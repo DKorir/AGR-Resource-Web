37ca29464122541b9edd1e3b820012107f94fd27
DKorir <dominic.korir@student.moringaschool.com>
2022-03-22 21:37:57 +0300

    create resourceform

--- Resource/urls.py
+++ Resource/urls.py
@@ -3,6 +3,6 @@ from .views import Home
 from . import views
 urlpatterns=[ 
     path('',views.Home.as_view(), name='home'),
-    path('resources/',views.resources_list, name='resources'),
-    path('resources/',views.resources_list, name='resources'),
+    path('resources',views.resources_list, name='resources'),
+    path('resources/upload',views.upload_resources, name='upload'),
 ]
--- Resource/views.py
+++ Resource/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .forms import ResourceForm
 class Home(TemplateView):
     template_name='home.html'
 def resources_list(request):
