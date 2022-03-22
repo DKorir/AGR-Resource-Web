6dbb3306b6d871cffb936d0d19eb2831389f9ee9
DKorir <dominic.korir@student.moringaschool.com>
2022-03-22 13:45:53 +0300

    add views

--- Resource/urls.py
+++ Resource/urls.py
@@ -1,6 +1,5 @@
-from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
+from .views import Home
 from . import views
 urlpatterns=[ 
     path('',views.Home.as_view(), name='home'),
--- Resource/views.py
+++ Resource/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+class Home(TemplateView):
+    template_name='home.html'
 def resources_list(request):
     return render(request,'resources_list.html')
 def upload_resources(request):
